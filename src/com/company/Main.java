498af9ffa6a253f55576624b869c55bbd5a37e80
ivand <ivandariorap1@outlook.es>
2022-07-07 20:39:07 -0500

    bucle do_while

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numeroWhile = 2;
+
+        do{
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }while(numeroWhile <3);
+    }
+}
